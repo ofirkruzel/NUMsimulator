e3bc4417fcf0e280f047e3f4d291678eccdfa1ff
ofirkruzel <ofir.kruzel@gmail.com>
2024-07-04 20:51:25 +0300

    user_interface

--- simulator.py
+++ simulator.py
@@ -1,13 +1,14 @@
 import time
 import matplotlib.pyplot as plt
+import num_interface
 
 def print_simulation_options():
-    print(f"2: solving NUM problem with primal algorithm with different alpha-fairness values")
-    print(f"3: solving NUM problem with dual algorithm with different alpha-fairness values")
-    print(f"4")
-    print(f"5")
-    print(f"6")
+    print(f"2: solving NUM problem with primal or dual algorithm with specific alpha")
+    print(f"4: solving NUM problem with Primal and dual and diffrent alpha values ")
+    print(f"5: solving NUM problem with Primal and dual and dijkastra distance")
+    print(f"6: solving NUM problem with Primal and dual and bellman ford distance")
     print(f"99: stop simulating")
+
 
 
 def selection():
@@ -18,6 +19,7 @@ def selection():
         print_simulation_options()
         selection = input(f"\nselection:")
     return int(selection)
+
 
 def get_alpha_input():
     while True:
@@ -35,22 +37,67 @@ def get_alpha_input():
             print("Invalid input. Please enter a number.")
 
 
+def get_algorithm_input():
+    while True:
+        try:
+            algo_input = int(input("Enter 1 for Primal algorithm or 2 for Dual algorithm: "))
+            if algo_input == 1:
+                return "Primal"
+            elif algo_input == 2:
+                return "Dual"
+            else:
+                print("Invalid input. Please enter either 1 or 2.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
-def active_Q2():
-    #primal algorithm
-    N = 5
-    Algo ="Primal"
+def get_valid_input(prompt, input_type, condition=lambda x: True, error_message="Invalid input."):
+    while True:
+        try:
+            value = input_type(input(prompt))
+            if condition(value):
+                return value
+            else:
+                print(error_message)
+        except ValueError:
+            print(error_message)
+
+def input_pram():
+    num_of_users = get_valid_input("Enter the number of users (N): ", int, condition=lambda x: x > 0,error_message="Number of users must be a positive integer.")
+
+    radius = get_valid_input("Enter the radius (M): ",float,condition=lambda x: x > 0,error_message="Radius must be a positive number.")
+
+    neighbors_radius = get_valid_input("Enter the neighbors' radius (R): ",float,condition=lambda x: x > 0,error_message="Neighbors' radius must be a positive number.")
+
+    return num_of_users,radius, neighbors_radius
+
+
+
+def active_Q2_Q3():
+    Algo = get_algorithm_input()
     alpha = get_alpha_input()
-    set_global_params(n=N)
-    Fun.calc_inter_face = False
-    network = Fun.Network(create_network_type="NUM")
-    network.draw_network()
-    print(network)
-    fun_function.CalcNetworkRate(network, alpha, Algo)
-    plt.show()  # to stop the code so we can analyze the graph showing the rates
+    N,M,R=input_pram()
+    num_interface.num(alpha,num_of_users=N, radius=M, neighbors_radius=R,Algo=Algo )
 
+def active_Q4():
+    Algo = get_algorithm_input()
+    alpha = [1,2,float("inf")]
+    N,M,R=input_pram()
+    for a in alpha:
+        num_interface.num(a,num_of_users=N, radius=M, neighbors_radius=R,Algo=Algo )
+    
+def active_Q5():
+    Algo = get_algorithm_input()
+    alpha = get_alpha_input()
+    N,M,R=input_pram()
+    num_interface.dijkastra(alpha,num_of_users=N, radius=M, neighbors_radius=R,Algo=Algo )
 
-Q_active_functions_dict = {2: active_Q2, 3: active_Q3, 4: active_Q4, 5: active_Q5, 6: active_Q6}
+def active_Q6():
+    Algo = get_algorithm_input()
+    alpha = get_alpha_input()
+    N,M,R=input_pram()
+    num_interface.bellman_ford(alpha,num_of_users=N, radius=M, neighbors_radius=R,Algo=Algo )
+
+Q_active_functions_dict = {2: active_Q2_Q3, 4: active_Q4, 5: active_Q5, 6: active_Q6}
 
 if __name__ == "__main__":
     print(f"Welcome to NUM simulator")
